f6e947b36a9caedec9fadb04a6fc9157a5b2fe59
László Nagy <laszlo.nagy@p92.hu>
2019-12-05 16:33:39 +0100

    -

--- src/main/com/jenkins/run/Run.java
+++ src/main/com/jenkins/run/Run.java
@@ -3,9 +3,11 @@ package com.jenkins.run;
 public class Run {
 
 	public static void main(String[] args) {
+
 		System.out.println("Hello world");
 		System.out.println("Hello branch-1");
-		"Hello".equals("Hello");	
+		"Hello".equals("Hello");
+		"Hello".equals("Hello");
 	}
 
 }
